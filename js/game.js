369532b828c1c97193a336de1f89b3860e81244e
Aaron McLeod <sircoolguy99@gmail.com>
2013-08-23 22:47:47 -0400

    setup the basics

--- js/game.js
+++ js/game.js
@@ -1,42 +1,29 @@
-
-/* Game namespace */
 var game = {
-  // Run on page load.
-  "onload" : function () {
-    // Initialize the video.
-    if (!me.video.init("screen", 480, 320, true, 'auto')) {
+  onload: function () {
+    if (!me.video.init("screen", 960, 640, true, '1')) {
       alert("Your browser does not support HTML5 canvas.");
       return;
     }
   
-  // add "#debug" to the URL to enable the debug Panel
-  if (document.location.hash === "#debug") {
-    window.onReady(function () {
-    me.plugin.register.defer(debugPanel, "debug");
-    });
-  }
+    if (document.location.hash === "#debug") {
+      window.onReady(function () {
+      me.plugin.register.defer(debugPanel, "debug");
+      });
+    }
 
-    // Initialize the audio.
     me.audio.init("mp3,ogg");
-
-    // Set a callback to run when loading is complete.
     me.loader.onload = this.loaded.bind(this);
-   
-    // Load the resources.
     me.loader.preload(game.resources);
-
-    // Initialize melonJS and display a loading screen.
     me.state.change(me.state.LOADING);
   },
 
 
 
-  // Run on game resources loaded.
-  "loaded" : function () {
+  loaded : function () {
     me.state.set(me.state.MENU, new game.TitleScreen());
     me.state.set(me.state.PLAY, new game.PlayScreen());
-
-    // Start the game.
+    me.sys.gravity = 0;
+    me.entityPool.add("player", game.Player);
     me.state.change(me.state.PLAY);
   }
 };
--- js/screens/play.js
+++ js/screens/play.js
@@ -3,7 +3,17 @@ game.PlayScreen = me.ScreenObject.extend({
    *  action to perform on state change
    */
   onResetEvent: function() {  
-    ; // TODO
+    me.levelDirector.loadLevel('level');
+    me.input.bindKey(me.input.KEY.W, 'up');
+    me.input.bindKey(me.input.KEY.A, 'left');
+    me.input.bindKey(me.input.KEY.S, 'down');
+    me.input.bindKey(me.input.KEY.D, 'right');
+    me.input.bindKey(me.input.KEY.UP, 'up');
+    me.input.bindKey(me.input.KEY.LEFT, 'left');
+    me.input.bindKey(me.input.KEY.DOWN, 'down');
+    me.input.bindKey(me.input.KEY.RIGHT, 'right');
+
+    me.game.world.addChild(new game.Darkness(32, 32));
   },
   
   
@@ -11,6 +21,13 @@ game.PlayScreen = me.ScreenObject.extend({
    *  action to perform when leaving this screen (state change)
    */
   onDestroyEvent: function() {
-  ; // TODO
+    me.input.unbindKey(me.input.KEY.W);
+    me.input.unbindKey(me.input.KEY.A);
+    me.input.unbindKey(me.input.KEY.S);
+    me.input.unbindKey(me.input.KEY.D);
+    me.input.unbindKey(me.input.KEY.UP);
+    me.input.unbindKey(me.input.KEY.LEFT);
+    me.input.unbindKey(me.input.KEY.DOWN);
+    me.input.unbindKey(me.input.KEY.RIGHT);
   }
 });
