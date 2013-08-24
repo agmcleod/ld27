ad3c1b7058c1d79a3b6ba20858c60635b3214976
Aaron McLeod <sircoolguy99@gmail.com>
2013-08-23 22:51:52 -0400

    moved darkness to entity folder

--- js/entities/darkness.js
+++ js/entities/darkness.js
@@ -1,5 +1,10 @@
-game.Darkness = me.SpriteObject.extend({
+game.Darkness = me.ObjectEntity.extend({
   init: function(x, y) {
+    var settings = {
+      image: 'darkness',
+      spritewidth: 960,
+      spriteheight: 640
+    };
     this.parent(x, y, settings);
     this.z = 200;
     this.flashed = false;
--- js/resources.js
+++ js/resources.js
@@ -1,5 +1,6 @@
 game.resources = [
   { name: 'level', type: 'tmx', src: 'data/map/level.tmx' },
   { name: 'simplebg', type: 'image', src: 'data/img/simplebg.png' },
-  { name: 'player', type: 'image', src: 'data/img/simplecharacter.png' }
+  { name: 'player', type: 'image', src: 'data/img/simplecharacter.png' },
+  { name: 'darkness', type: 'image', src: 'data/img/darkness.png' }
 ];
