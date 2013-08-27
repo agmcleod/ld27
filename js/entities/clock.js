5c6fbbf9011499a0c736fe0d2b6496a437c49c4b
Aaron McLeod <aaron.mcleod@mosaic.com>
2013-08-27 15:21:08 -0400

    changed clock to renderable type

--- js/entities/clock.js
+++ js/entities/clock.js
@@ -1,12 +1,9 @@
-game.Clock = me.ObjectEntity.extend({
+game.Clock = me.Renderable.extend({
   init: function() {
     this.x = me.game.viewport.width - 80;
     this.y = 30;
-    this.parent(this.x, this.y, {
-      image: 'clock',
-      spritewidth: 64, 
-      spriteheight: 64
-    });
+    this.parent(new me.Vector2d(this.x, this.y), 64, 64);
+    this.renderable = new me.AnimationSheet(0,0, me.loader.getImage('clock'), 64, 64);
     this.renderable.addAnimation('0', [0], 1);
     this.renderable.addAnimation('1', [1], 1);
     this.renderable.addAnimation('2', [2], 1);
@@ -20,5 +17,12 @@ game.Clock = me.ObjectEntity.extend({
     this.renderable.setCurrentAnimation('0');
     this.floating = true;
     this.z = 500;
+  },
+
+  draw: function(context) {
+    this.parent(context);
+    context.translate(this.pos.x, this.pos.y);
+    this.renderable.draw(context);
+    context.translate(-this.pos.x, -this.pos.y);
   }
 });
