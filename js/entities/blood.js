210532ecef4c1857e15d1d297122afdd4a105dd2
Aaron McLeod <aaron.mcleod@mosaic.com>
2013-08-27 15:11:45 -0400

    changed the spikes & blood to extend renderable

--- js/entities/blood.js
+++ js/entities/blood.js
@@ -1,10 +1,8 @@
-game.Blood = me.ObjectEntity.extend({
+game.Blood = me.Renderable.extend({
   init: function(x, y) {
-    this.parent(x, y, {
-      image: 'trap',
-      spritewidth: 32,
-      spriteheight: 32
-    });
+    var w = 32, h = 32;
+    this.parent(new me.Vector2d(x, y), w, h);
+    this.renderable = new me.AnimationSheet(0, 0, me.loader.getImage('trap'), w, h);
     this.renderable.addAnimation('splat', [5,5,5,5,10,11,12,13,14], 30);
     var _this = this;
     this.renderable.setCurrentAnimation('splat', function() {
@@ -12,5 +10,12 @@ game.Blood = me.ObjectEntity.extend({
       return false;
     });
     this.z = 220;
+  },
+
+  draw: function(context) {
+    this.parent(context);
+    context.translate(this.pos.x, this.pos.y);
+    this.renderable.draw(context);
+    context.translate(-this.pos.x, -this.pos.y);
   }
 });
--- js/entities/spikes.js
+++ js/entities/spikes.js
@@ -1,10 +1,8 @@
-game.Spikes = me.ObjectEntity.extend({
+game.Spikes = me.Renderable.extend({
   init: function(x, y, trapParent) {
-    this.parent(x, y-32, {
-      image: 'trap',
-      spritewidth: 32,
-      spriteheight: 64
-    });
+    var w = 32, h = 64;
+    this.parent(new me.Vector2d(x, y-32), w, h);
+    this.renderable = new me.AnimationSheet(0, 0, me.loader.getImage('trap'), w, h);
     this.renderable.addAnimation('fire', [1,2,3,4,4,4,4,3,2,1], 30);
     var _this = this;
     this.renderable.setCurrentAnimation('fire', function() {
@@ -12,5 +10,12 @@ game.Spikes = me.ObjectEntity.extend({
       trapParent.collided = false;
     });
     this.z = 220;
+  },
+
+  draw: function(context) {
+    this.parent(context);
+    context.translate(this.pos.x, this.pos.y);
+    this.renderable.draw(context);
+    context.translate(-this.pos.x, -this.pos.y);
   }
 });
